e26b9a2cbd2f3498622b1ad112c6f080baaa00b4
Ezekieladeti <159878539+Ezekieladeti@users.noreply.github.com>
2024-04-09 05:29:28 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import teachers
+import students
+
+
+student_names = ["Sam", "Job", "Ken", "Eze", "James"]
+
+teachers.print_name("Oswago")
+print("\n")
+teachers.print_course("Kango ","Physics")
+
+students.print_name("Ezekiel")
+print("\n")
+
+students.print_student_names(student_names)
