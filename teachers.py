44c3a052eee82749fc520940f030b057e0a525f9
Ezekieladeti <159878539+Ezekieladeti@users.noreply.github.com>
2024-04-09 05:31:49 -0700

    Add files via upload

---
+++ teachers.py
@@ -0,0 +1,5 @@
+def print_name(name):
+    print("My name is {Ezekiel Adeti}".format(name))
+
+def print_course(name,course):
+    print("Dr {Oswago} teaches {Medicine} at JKUAT".format(name,course))
