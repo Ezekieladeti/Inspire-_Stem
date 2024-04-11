f3244b2885a3cc679da6c4ae9f674a0596b6c908
Ezekiel Adeti <159878539+Ezekieladeti@users.noreply.github.com>
2024-04-11 14:32:19 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+# install tkinter  and pyserial
+# pip install pyserial
+# pip install tk
+
+# basic hello world
+
+from tkinter import *
+
+window = Tk()
+
+window.title("Sensors Tutorial")
+
+window.mainloop()
